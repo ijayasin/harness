8e5fb15d40a57fa742a509abc2d112de5355277e
Indika Jayasinghe <indika@brightroll.com>
2012-02-28 11:39:05 -0800

    Updated values in the MODIFY_ECHO_PARAMS data structure.

--- test_harness/stats_writer.rb
+++ test_harness/stats_writer.rb
@@ -1,10 +1,25 @@
 require 'yaml'
 
+
+# Reading YAML
+#   http://juixe.com/techknow/index.php/2009/10/08/jamming-with-ruby-yaml/
 module TestHarness
   class StatsWriter
 
-    def initialize(start_time)
+    def initialize(start_time, path)
       
+    end
+
+    def save(obj)
+      File.open( 'animals.yaml', 'w' ) do |out|
+        YAML.dump( ['badger', 'elephant', 'tiger'], out )
+      end
+    end
+
+    protected
+
+    def filename
+      File.join(path, "#{}")
     end
 
   end
