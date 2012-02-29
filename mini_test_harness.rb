f86017f6ed6eb95c74a2dd62261bd2409e69b141
Martin Brown <martin@brightroll.com>
2012-02-28 17:15:50 -0800

    Changs

--- mini_test_harness.rb
+++ mini_test_harness.rb
@@ -9,46 +9,42 @@ require 'test_harness/stats_writer'
 module TestHarness
 class Runner
 
-  INPUTS  = Dir.glob("/Users/martinbrown/git/harness/inputs/*").freeze
-  CLIENTS = Dir.glob("/Users/martinbrown/git/api-rally/*/*\.sh").freeze
-  ITERATIONS = [10,100,1000,10000,100000]
+  INPUTS  = Dir.glob("/opt/apirally/harness/inputs/*").freeze
+  
+  rally_dir = "/opt/apirally/api-rally/"
+#CLIENTS = [File.join(rally_dir,"messagepack_node/client.sh")]
+#CLIENTS = [File.join(rally_dir,"messagepack_node/client.sh"),
+#	   File.join(rally_dir,"no_scrubs/lovesocket")]
+CLIENTS = [File.join(rally_dir,"no_scrubs/lovesocket")]  
+# File.join(rally_dir, "rest_json_c/client.rb")]
+
+  SERVERS = [File.join(rally_dir, "no_scrubs/server.js")]
+
+  RUNNERS = ['node']
+  ITERATIONS = [10,100,1000,10000]
 
   def initialize
-    CLIENTS.each do |client|
-      puts client
-    end
-    @start_time = Time.now
-    @stats = Hash.new do |h,k|
-      h[k] = Hash.new{|hh,kk| hh[kk] = {} }
-    end
-  end
-
-  def clients
-    clnts = (@options && @options.clients)
-    clnts.reject{|v| v.nil? || v.empty?} if clnts
-    clnts || CLIENTS
-  end
-
-  def cases
-    if @options && @options.test_cases
-      @options.test_cases.reject{|v| v.nil? || v.emtpy?}
-    else
-      TEST_CASES.keys
-    end
   end
 
   def serialize(params)
     t = Time.now.utc
-    %x("./#{params['client']} --serialize --object #{params['object']} -n #{params['iterations']} -f #{params['input']}")
+    puts "CWD: #{Dir.getwd()}"
+    puts "INPUT: #{params['input']}"
+      system("./#{params['client']} --serialize --object #{params['object']} -n #{params['iterations']} -f #{params['input']} > #{File.basename(params['input'])}.serialized") 
     Time.now.utc - t
   end
 
   def deserialize(params)
     t = Time.now.utc
-    %x("#{params['client']} --deserialize --object #{params['object']} -n #{params['iterations']} -f #{params['input']}")
+    system("./#{params['client']} --deserialize --object #{params['object']} -n #{params['iterations']} -f #{File.basename(params['client'])}.serialized")
     Time.now.utc - t
   end
 
+  def echo(params)
+    t = Time.now.utc
+    system("bash #{params['client']} --echo --object #{params['object']} --host localhost -n #{params['iterations']} -f #{params['input']}")
+    Time.now.utc - t
+  end
 
   def run(args=nil)
 
@@ -56,39 +52,44 @@ class Runner
       h[k] = Hash.new{|hh,kk| hh[kk] = {} }
     end
 
-    clients.each do |client|
+    CLIENTS.each do |client|
       params = {}
       Dir.chdir(File.dirname(client)) do
+      	system("node #{File.basename(SERVERS[0])} &")
         params['client'] = File.basename(client)
         INPUTS.each do |input|
           params['object'] = File.basename(input)
           params['input'] = input
           ITERATIONS.each do |iteration|
             params['iterations'] = iteration
-            stats[File.basename(params['client'])]['serialize']["#{params['input']}-#{iteration}"] = serialize(params)
-            stats[File.basename(params['client'])]['deserialize']["#{params['input']}-#{iteration}"] = deserialize(params)
+            #stats[File.basename(params['client'])]['serialize']["#{File.basename(params['input'])}-#{iteration}"] = serialize(params)
+            #stats[File.basename(params['client'])]['deserialize']["#{File.basename(params['input'])}-#{iteration}"] = deserialize(params)
+            stats[File.basename(params['client'])]['echo']["#{File.basename(params['input'])}-#{iteration}"] = echo(params)
           end
         end
       end
     end
-
-    pp stats
-    StatsWriter.new(@start_time, :path=>@options.stats_path).save(@stats)
-  end
-
-  def stats
-    @stats
+    graph_results(stats)
   end
 
   def graph_results(results)
+    input_cases = []
+    INPUTS.each do |input|
+      ITERATIONS.each do |iteration|
+        input_cases << "#{File.basename(input)}-#{iteration}"
+      end
+    end
     template_file = "results.html.erb"
     template = Erubis::Eruby.load_file(template_file)
-    template.result({
-      :clients => CLIENTS.collect { |client| File.basename(client) },
-      :tests => TEST_CASES.keys,
-      :objects => MODIFY_ECHO_PARAMS.collect {|p| p['type']},
-      :data => data # data is a nested hash: data[client][test][object]
-    })
+    File.open('results.html', 'w') do |f|
+      result = template.result({
+        :clients => CLIENTS.collect { |client| File.basename(client) },
+        :tests => ['serialize','deserialize'],
+        :inputs => input_cases,
+        :data => results # data is a nested hash: data[client][test][object]
+      })
+      f.write result
+    end
   end
 
 end
